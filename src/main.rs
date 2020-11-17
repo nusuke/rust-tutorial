c398c43b70f6ea7cf81ef79741feb957773977b9
nusuke <nusukep@gmail.com>
2020-11-17 21:45:55 +0900

    :tada: module

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}",Module::Module2::isOdd(10));
+    println!("{}",ModuleA::isEven(10));
+}
+
+mod Module {
+    pub mod Module2{
+        pub fn isOdd(num: i32) -> bool{
+            return num %2 == 0;
+
+        }
+    }
+}
+
+mod ModuleA {
+    pub fn isEven(num :i32) -> bool{
+        return !crate::Module::Module2::isOdd(num)
+    }
 }
